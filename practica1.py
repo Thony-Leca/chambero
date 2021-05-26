1db7f2b0caba78ac669b0919ac6a008410b60806
Thony-Leca <antholeca45@gmail.com>
2021-05-26 09:56:20 -0500

    Create practica1.py

---
+++ practica1.py
@@ -0,0 +1,7 @@
+#Ingrese por pantalla su nombre y su carrera
+nombre=input( "Ingrese su nombre : " ) 
+edad=int( input( "Ingrese su edad : ")) 
+carrera=input('Ingrese su carrera : ') 
+print(nombre) 
+print(edad) 
+print(carrera)
