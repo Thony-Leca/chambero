af7c8928ac68ac612ddd5998725abfc38abb44bb
Thony-Leca <antholeca45@gmail.com>
2021-05-26 09:56:57 -0500

    Create practica6.py

---
+++ practica6.py
@@ -0,0 +1,9 @@
+#EJERCICIO1:Mostrar los numeros del 1 al 10.
+for i in [1,2,3,4,5,6,7,8,9,10]:
+	print(i)
+#EJERCICIO2: Mostrar la palabra CERTUS 5 veces
+for i in [1,2,3,4,5]:
+	print('CERTUS')
+	
+for i in range(1,101):
+	print(i,'RAAA')
