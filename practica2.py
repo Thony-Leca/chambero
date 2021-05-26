6fd0741f666d480f81f434515b437aeb5cdda7c9
Thony-Leca <antholeca45@gmail.com>
2021-05-26 09:56:48 -0500

    Create practica2.py

---
+++ practica2.py
@@ -0,0 +1,8 @@
+#Crear una aplicacion que nos permita
+#cambiar soles a dolares
+soles=float(input('Ingrese la cantidad de soles: ' ) )
+dolares=soles*3.61
+print('===============CERTUS==================')
+print('soles: ', soles)
+print('dolares: ', dolares)
+print('===============CERTUS==================')
